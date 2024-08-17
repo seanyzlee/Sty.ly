4bbdd40e040b83a9ec3e5e92d9302b8430e9c649
August <sjsaug@gmail.com>
2024-08-17 01:52:22 -0400

    better

--- ML/gen_dataset.py
+++ ML/gen_dataset.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 PROMPT_TEMPLATE = """
 You have been given example entries from a dataset. Please generate {entries} more entries that are similar to the given examples.
+Do not use markdown format.
 Data: 
 {data}
 """
@@ -12,8 +13,8 @@ def new_dataset(entries, data):
     return ollama.chat(
         model='llama3.1',
         messages=[{'role': 'user', 'content': ChatPromptTemplate.from_template(PROMPT_TEMPLATE).format(entries=str(entries), data=data)}],
-        stream=False,
-        )
+        stream=False
+        )['message']['content']
 
 def csv_to_json(csv):
     return pd.read_csv(csv).to_json('ML/datasets/weatherHistory_clean_rain.json', orient='records', lines=False)
@@ -25,7 +26,7 @@ def read_json(json):
 def main():
     csv_to_json('ML/datasets/weatherHistory_clean_rain.csv')
     data = read_json('ML/datasets/weatherHistory_clean_rain.json')
-    new_data = new_dataset(50, data)
+    new_data = new_dataset(480, data)
     print(new_data)
 
 if __name__ == '__main__':
